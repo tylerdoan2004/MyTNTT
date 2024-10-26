7c2301f0b2d8e626014255bdf1bebdc9f33cd342
Tyler Doan <tylerqud@uci.edu>
2024-10-25 17:57:45 -0700

    Initial Commit

---
+++ MyTNTT/MyTNTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTNTTApp.swift
+//  MyTNTT
+//
+//  Created by Tyler Doan on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTNTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
